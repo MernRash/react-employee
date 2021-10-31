457e6fb5f6a5c3b1c65e419e09323d193104db8b
MernRash <therahul.shrestha72744@gmail.com>
2021-10-31 23:33:25 +0530

    React Employee App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import EmployeeForm from './Components/EmployeeForm/EmployeeDetails'
+
+function App() {
+  return (
+   <div>
+     <EmployeeForm />
+   </div>
+  );
+}
+
+export default App;
